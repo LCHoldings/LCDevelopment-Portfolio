73c30daabdce5d494c5ad3e01c7ba51738eb437d
Lazyllama <me@lazyllama.xyz>
2024-08-02 01:07:24 +0200

    Feat | Barebones setup.
    This took way too long since I had to restart Nuxt every time I changed the ENV. But it works now so let's get to the fun part!

---
+++ studio/sanity.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {presentationTool} from 'sanity/presentation'
+import {visionTool} from '@sanity/vision'
+
+import {schemaTypes} from './schemas'
+
+export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
+export const dataset = process.env.SANITY_STUDIO_DATASET!
+
+export default defineConfig({
+  name: 'project-name',
+  title: 'Project Name',
+  projectId,
+  dataset,
+  plugins: [
+    structureTool(),
+    presentationTool({
+      previewUrl: {
+        origin: process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000',
+        previewMode: {
+          enable: '/preview/enable',
+          disable: '/preview/disable',
+        },
+      },
+    }),
+    visionTool(),
+  ],
+  schema: {
+    types: schemaTypes,
+  },
+})
